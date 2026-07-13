578174da7d7ac6cca5715569c32528c15a23d955
Khadeeja Memon <memon12787@gmail.com>
2026-07-13 13:41:47 +0500

    fixed

--- spotify-backend/config/db.js
+++ spotify-backend/config/db.js
@@ -3,19 +3,17 @@ const mongoose = require("mongoose");
 let isConnected = false;
 
 const connectDB = async () => {
-  if (isConnected) {
-    return;
-  }
+  if (isConnected) return;
 
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 10000,
-    });
-    isConnected = conn.connections[0].readyState === 1;
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    isConnected = conn.connection.readyState === 1;
+
     console.log("MongoDB Connection successful");
   } catch (error) {
-    console.error("MongoDB Connection Failed");
-    console.error(error.message);
+    console.error("MongoDB Connection Failed:", error);
+    throw error; // error ko suppress mat karein
   }
 };
 
