28f91e17d22b84060fd25c1a00b9e6e8c8c0af24
Khadeeja Memon <memon12787@gmail.com>
2026-07-22 19:28:40 +0500

    fixed

--- spotify-backend/routes/aiRoutes.js
+++ spotify-backend/routes/aiRoutes.js
@@ -3,9 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const { getRecommendations } = require("../controllers/aiController");
-
-const { protect } = require("../middleware/authMiddleware");
-
+const protect = require("../middleware/authMiddleware");
 // AI Song Recommendations
 router.get("/recommend", protect, getRecommendations);
 
